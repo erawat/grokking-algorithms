0321cef39c758d57987162d04a2e2f58e4409627
Erawat Chamanont <erawat@gmail.com>
2021-08-22 19:00:58 +0100

    Add BinarySearchTest

---
+++ src/test/java/algorithms/BinarySearchTest.java
@@ -0,0 +1,16 @@
+package algorithms;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+
+class BinarySearchTest {
+
+  @Test
+  void binarySearch() {
+    BinarySearch bs = new BinarySearch();
+    int[] array = {2, 5, 6, 10, 20, 21, 25};
+    assertEquals(0, bs.binarySearch(array, 2));
+    assertEquals(-1, bs.binarySearch(array, 15));
+  }
+}
