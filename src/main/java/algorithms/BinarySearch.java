b33687c7e9cc7e5ca7e09b7bca3fd94528b6b4b6
Erawat Chamanont <erawat@gmail.com>
2021-08-22 18:59:28 +0100

    Move and format  BinarySearch class

---
+++ src/main/java/algorithms/BinarySearch.java
@@ -0,0 +1,26 @@
+package algorithms;
+
+/**
+ * Iterative Binary Search.
+ */
+public class BinarySearch {
+
+  public int binarySearch(int[] list, int item) {
+    int low = 0;
+    int high = (list.length - 1);
+    while (low <= high) {
+      int mid = (low + high) / 2;
+      int guess = list[mid];
+      if (guess == item) {
+        return mid;
+      }
+      if (guess > item) {
+        high = mid - 1;
+      } else {
+        low = mid + 1;
+      }
+    }
+
+    return -1;
+  }
+}
